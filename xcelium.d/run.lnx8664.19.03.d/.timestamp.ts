06e8359fb5c22f252334b3485d0d64fa14561722
David Garcia Gonzalez <dgarci23@nd.edu>
2023-04-22 22:00:16 -0400

    Lab 5 finished

---
+++ xcelium.d/run.lnx8664.19.03.d/.timestamp.ts
@@ -0,0 +1,1 @@
+1597989678 /escnfs/courses/fa22-cse-40462.01/dropbox/dgarci23/mips8/processor_multi.sv
---
+++ xcelium.d/run.lnx8664.19.03.d/files.ts
@@ -0,0 +1,2 @@
+1682113859 /escnfs/courses/fa22-cse-40462.01/dropbox/dgarci23/mips8/cds.lib
+1597989678 /escnfs/courses/fa22-cse-40462.01/dropbox/dgarci23/mips8/processor_multi.sv
